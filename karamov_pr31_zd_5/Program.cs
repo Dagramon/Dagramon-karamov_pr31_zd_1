c2c7ffbb471607622f47a57df20b7ec15314c724
Dagramon <karamov-damir228@mail.ru>
2025-11-12 11:41:16 +0500

    initial

---
+++ karamov_pr31_zd_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace karamov_pr31_zd_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("input.txt"))
+            {
+                File.CreateText("input.txt");
+            }
+            string[] lines = File.ReadAllLines("input.txt");
+            string[] first = lines[0].Split(' ');
+            int N = int.Parse(first[0]);
+            int C = Convert.ToInt32(first[1]);
+            int phones = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                int voters = int.Parse(lines[i]);
+                int needed = voters / 2 + 1;
+                phones += needed;
+            }
+            File.WriteAllText("output.txt", $"{phones * C}");
+        }
+    }
+}
